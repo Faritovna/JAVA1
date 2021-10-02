3e346431bc28c83e644fc18358a603a7785f64e8
Aliya Sharapova <sharapovaaliya@gmail.com>
2021-10-02 16:14:47 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account_balance =  2_000_000_000;
+        int tracfer_ammount = 500_000_000;
+        int total = account_balance + tracfer_ammount;
+        System.out.println(total);
+    }
+}
